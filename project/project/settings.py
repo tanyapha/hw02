e74620673afc5f33c3d717bc55ab4381c91bc9c6
tanyapha <tanyakpis@gmail.com>
2022-03-04 14:35:48 -0500

    changed to local dbsql database

--- project/project/settings.py
+++ project/project/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'songraterdb',
-        'USER': 'test',
-        'PASSWORD':"software_COMP333",
-        'HOST':'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- project/songrater/models.py
+++ project/songrater/models.py
@@ -6,9 +6,15 @@ class Users(models.Model):
                                 unique=True)
     password = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.username
+
 class Artist(models.Model):
     song = models.CharField(max_length=255, unique=True, primary_key= True)
     artist = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.song
 
 class Ratings(models.Model):
     username = models.ForeignKey(Users, on_delete = models.CASCADE)
