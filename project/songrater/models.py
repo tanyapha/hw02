ff48556e4c429405094bbd6fe2f0e977f6345835
tanyapha <tanyakpis@gmail.com>
2022-03-08 22:29:34 -0500

    made artist made a foriegn key in artist

--- project/songrater/models.py
+++ project/songrater/models.py
@@ -11,9 +11,13 @@ class Users(models.Model):
     def __str__(self):
         return self.username
 
+class ArtistInformation(models.Model):
+    artist_name = models.CharField(max_length=255, unique=True, primary_key= True)
+    birth_day = models.DateField()
+
 class Artist(models.Model):
     song = models.CharField(max_length=255, unique=True, primary_key= True)
-    artist = models.CharField(max_length=255)
+    artist = models.ForeignKey(ArtistInformation, on_delete= models.CASCADE)
 
     def __str__(self):
         return self.song
@@ -22,11 +26,6 @@ class Ratings(models.Model):
     username = models.ForeignKey(Users, on_delete = models.CASCADE)
     song = models.ForeignKey(Artist, on_delete= models.CASCADE)
     rating = models.IntegerField(max_length=1)
-
-class ArtistInformation(models.Model):
-    artist = models.CharField(max_length=255, unique=True, primary_key= True)
-    birth_day = models.DateField()
-
 
 class Albums(models.Model):
     title = models.CharField(max_length=255)
