53a5cf3e747a545e7c0522ad0cdde75f01670c63
tanyapha <tanyakpis@gmail.com>
2022-03-09 00:08:41 -0500

    working search forms

--- project/songrater/views.py
+++ project/songrater/views.py
@@ -7,20 +7,29 @@ from .models import Users, Ratings, Artist
 
 def home(request):
     context = {'message':''}
-    if request.method == "POST":
-        name_count = Users.objects.filter(username=request.POST['Username']).count()
+    username_input = request.POST.get('username-register')
+    # val = request.POST
+    # context = {'message':val}
+    if 'register' in request.POST:
+        name_count = (Users.objects.filter(username=username_input)).count()
         if name_count == 0:
-            user=Users(username = request.POST['Username'], password = request.POST['Password'])
+            user = Users(username = username_input, password = request.POST.get('password'))
             user.save()
             context = {'message':'Successfully registered.'}
         else:
             context = {'message':'This username already exists.'}
+    elif 'retrieve' in request.POST:
+        username_input = request.POST.get('username-songs')
+        ratings = Ratings.objects.filter(username_id=username_input).values("song_id","rating")
+        # val = request.POST
+        context = {'ratings':ratings}
     return render(request, 'home.html', context)
 
 def retrieval(request, username_id):
-    ratings = get_object_or_404(Ratings, pk=username_id)
-    try:
-        ratings = Ratings.objects.get(pk=username_id)
-    except Ratings.DoesNotExist:
-        raise Http404("Username does not exist")
+    username_input = request.POST.get('username-songs')
+    ratings = Ratings.objects.filter(username_id=username_input)
+    # try:
+    #     ratings = Ratings.objects.get(username_id=username_id)
+    # except Ratings.DoesNotExist:
+    #     raise Http404("Username does not exist")
     return render(request, 'home.html', {'ratings': ratings})
