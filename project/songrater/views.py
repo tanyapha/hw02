5bcb03b5ef96caf7ddd3110e326e55abe1da15d2
tanyapha <tanyakpis@gmail.com>
2022-03-09 12:57:39 -0500

    error messages for song/username + album/artist

--- project/songrater/views.py
+++ project/songrater/views.py
@@ -5,6 +5,12 @@ from .models import Users, Ratings, Artist, Albums
 
 # User Registration "home" page – displays user registration interface
 
+def ifExist(output, label, message):
+    if output[label].count() == 0:
+        return {label+'_error':message}
+    else:
+        output
+
 def home(request):
     context = {'message':''}
     if 'register' in request.POST:
@@ -18,18 +24,16 @@ def home(request):
             context = {'message':'This username already exists.'}
     elif 'retrieve' in request.POST:
         username_input = request.POST.get('username-songs')
-        ratings = Ratings.objects.filter(username_id=username_input).values("song_id","rating")
-        # val = request.POST
-        context = {'ratings':ratings}
+        rating = Ratings.objects.filter(username_id=username_input).values("song_id","rating")
+        if rating.count() == 0:
+            context["ratings_error"] = "No username found"
+        else:
+            context["ratings"] = rating
     elif 'retrieve-by-artist' in request.POST:
-        artist_input = request.POST.get('artist-name')
-        context = {'albums':Albums.objects.filter(artist_id = artist_input).values("title","number_of_songs")}
+        artist_input = (request.POST.get('artist-name')).title()
+        artist_albums = {"artist": artist_input,'albums':Albums.objects.filter(artist_id = artist_input).values("title","number_of_songs")}
+        if artist_albums['albums'].count == 0:
+            context["albums_error"] = 'No albums found'
+        else:
+            context = artist_albums
     return render(request, 'home.html', context)
-def retrieval(request, username_id):
-    username_input = request.POST.get('username-songs')
-    ratings = Ratings.objects.filter(username_id=username_input)
-    # try:
-    #     ratings = Ratings.objects.get(username_id=username_id)
-    # except Ratings.DoesNotExist:
-    #     raise Http404("Username does not exist")
-    return render(request, 'home.html', {'ratings': ratings})
